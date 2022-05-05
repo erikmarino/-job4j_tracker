2fcea59b5f417dc0f3c08f5ba788b3e4736b2520
erikmarino <eriksun86@mail.ru>
2022-05-05 12:58:06 +0300

    1.1. Создание объекта

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Dog {
+    /**
+     * в методе main создаём 3 объекта класса Dog, присваиваем им имена polkan, sharik, zhychka.
+     */
+    public static void main(String[] agrs) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
