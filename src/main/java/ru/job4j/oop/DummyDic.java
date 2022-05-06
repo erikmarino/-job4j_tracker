16280a519f56eacaebfe985946268310684381c4
erikmarino <eriksun86@mail.ru>
2022-05-06 18:59:12 +0300

    1.6. Взаимодействие объектов

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -1,5 +1,11 @@
 package ru.job4j.oop;
 
+/**
+ * 1. Создайте класс ru.job4j.oop.DummyDic.
+ * 2. Создайте в нем метод public String engToRus(String eng).
+ * 3. Метод должен вернуть строчку "Неизвестное слово. " + eng.
+ */
+
 public class DummyDic {
     public String engToRus(String eng) {
         return "Неизвестное слово." + eng;
