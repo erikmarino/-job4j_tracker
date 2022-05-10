4a9da5c1ebcd2d1126a7a8d6ef9312326ea7a532
erikmarino <eriksun86@mail.ru>
2022-05-10 17:16:30 +0300

    4. Расстояние между точками в трехмерном пространстве.

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -5,31 +5,37 @@ import static java.lang.Math.pow;
 
 public class Point {
 
-    /**
-     * Это поле объекта. Оно доступно только конкретному объекту.
-     */
     private int x;
-
-    /**
-     * И это поле объекта. Оно доступно только конкретному объекту.
-     */
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
+    }
+
+    public double distance3d(Point that) {
+        return sqrt((pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2)));
+    }
+
     public static void main(String[] args) {
-        Point a = new Point(3, 4);
-        Point b = new Point(1, 2);
-        double dist = a.distance(b);
-        System.out.println(dist);
+        Point a = new Point(3, 4, 6);
+        Point b = new Point(1, 2, 2);
+        double dist3 = a.distance3d(b);
+        System.out.println(dist3);
     }
 }
-
-
--- src/test/java/ru/job4j/tracker/PointTest.java
+++ src/test/java/ru/job4j/tracker/PointTest.java
@@ -7,21 +7,21 @@ import ru.job4j.oop.Point;
 public class PointTest {
 
     @Test
-    public void when00to20then2() {
-        double expected = 2.0;
-        Point a = new Point(0, 0);
-        Point b = new Point(2, 0);
-        double dist = a.distance(b);
-        Assert.assertEquals(expected, dist, 0.01);
+    public void when002to201then2dot23() {
+        double expected = 2.23;
+        Point a = new Point(0, 0, 2);
+        Point b = new Point(2, 0, 1);
+        double dist3 = a.distance3d(b);
+        Assert.assertEquals(expected, dist3, 0.01);
     }
 
     @Test
-    public void when12to34then2dot82() {
-        double expected = 2.82;
-        Point a = new Point(1, 2);
-        Point b = new Point(3, 4);
-        double dist = a.distance(b);
-        Assert.assertEquals(expected, dist, 0.01);
+    public void when346to122then4dot89() {
+        double expected = 4.89;
+        Point a = new Point(3, 4, 6);
+        Point b = new Point(1, 2, 2);
+        double dist3 = a.distance3d(b);
+        Assert.assertEquals(expected, dist3, 0.01);
     }
 
     @Test
