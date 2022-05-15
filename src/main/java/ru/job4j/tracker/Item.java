dffcacad7515e39d6bb399f846163d51e75b883e
erik <eriksun86@mail.ru>
2022-05-15 18:37:57 +0300

    7.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -9,8 +9,6 @@ public class Item {
     private int id;
     private String name;
 
-    private LocalDateTime created = LocalDateTime.now();
-
     public Item() {
     }
 
@@ -21,10 +19,6 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
     }
 
     public int getId() {
