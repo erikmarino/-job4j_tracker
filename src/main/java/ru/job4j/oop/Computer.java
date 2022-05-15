aa7e5616cc349766256dada95c95af8c64676451
erik <eriksun86@mail.ru>
2022-05-15 09:31:42 +0300

    3

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -12,6 +12,11 @@ public class Computer {
         this.cpu = cpu;
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
@@ -23,5 +28,7 @@ public class Computer {
         Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
         computer.printInfo();
         comp.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
     }
 }
