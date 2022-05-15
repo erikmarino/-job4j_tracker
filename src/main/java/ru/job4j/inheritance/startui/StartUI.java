26e295572d853325e18ef552e966299f32c0adce
erik <eriksun86@mail.ru>
2022-05-15 18:27:07 +0300

    7. Date. Отображение даты.

--- src/main/java/ru/job4j/inheritance/startui/StartUI.java
+++ src/main/java/ru/job4j/inheritance/startui/StartUI.java
@@ -11,3 +11,4 @@ public class StartUI {
         System.out.println(currentFormat);
     }
 }
+
--- src/main/java/ru/job4j/inheritance/usagepackagedate/UsagePackageDate.java
+++ src/main/java/ru/job4j/inheritance/usagepackagedate/UsagePackageDate.java
@@ -1,4 +1,4 @@
-package ru.job4j.inheritance.usagePackageDate;
+package ru.job4j.inheritance.usagepackagedate;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
