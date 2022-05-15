b4de7e7bcae22979dc95dfddb66f97ea43178382
erik <eriksun86@mail.ru>
2022-05-15 22:20:05 +0300

    2. Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,15 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 public class Item {
     private int id;
     private String name;
-
-    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -21,10 +14,6 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
     }
 
     public int getId() {
@@ -41,25 +30,6 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void printInfo() {
-        System.out.println(id);
-        System.out.println(name);
-    }
-
-    public static void main(String[] args) {
-        Item tempOne = new Item();
-        tempOne.printInfo();
-        Item tempTwo = new Item("Kate");
-        tempTwo.printInfo();
-        Item tempTree = new Item(13, "Stive");
-        tempTree.printInfo();
-
-        LocalDateTime createdOne = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = createdOne.format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
     }
 }
 
