1837f1eadedbcf9ef818b810aa89ffcd0671001c
erik <eriksun86@mail.ru>
2022-05-16 09:24:21 +0300

    2

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -42,5 +42,25 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public void printInfo() {
+        System.out.println(id);
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Item tempOne = new Item();
+        tempOne.printInfo();
+        Item tempTwo = new Item("Kate");
+        tempTwo.printInfo();
+        Item tempTree = new Item(13, "Stive");
+        tempTree.printInfo();
+
+        LocalDateTime createdOne = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = createdOne.format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+    }
 }
+
 
